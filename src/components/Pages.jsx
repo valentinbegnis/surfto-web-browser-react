ad1c53413d762c6662e6b0608323f626c5a8ad1f
valentin <jclyffe@DESKTOP-6EK9PFG.localdomain>
2022-05-16 13:03:29 -0300

    set default path to /search

--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -6,7 +6,7 @@ export const Pages = () => {
   return (
     <div className='p-4'>
       <Routes>
-        <Route path='/' element={<Navigate to='/search'/>} />
+        <Route path='/surfto-web-browser-react' element={<Navigate to='/search'/>} />
         <Route path='/search' element={<Results />} />
         <Route path='/image' element={<Results />} />
         <Route path='/news' element={<Results />} />
